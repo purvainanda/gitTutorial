cb901ab56faffb877eafc7bd976499c782a90a85
Purvai Nanda <unanda@uwo.ca>
2017-03-28 00:03:32 -0700

    Added Course class and made changes to Student and Instructor

---
+++ src/Course.java
@@ -0,0 +1,12 @@
+/**
+ * Created by purva on 2017-03-27.
+ */
+public class Course {
+    String cname;
+    String description;
+
+    public Course(String cname, String description) {
+        this.cname = cname;
+        this.description = description;
+    }
+}
